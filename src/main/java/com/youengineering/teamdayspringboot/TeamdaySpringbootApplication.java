21e2061bdc953a69191135eb9d1ea5380a2e31f4
Tobias Baumgartner <tobias.baumgartner@youengineering.com>
2019-05-31 11:43:22 +0200

    Initial Commit

---
+++ src/main/java/com/youengineering/teamdayspringboot/TeamdaySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.youengineering.teamdayspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamdaySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamdaySpringbootApplication.class, args);
+	}
+
+}
